353b41671ef501164a737837a1703b5b7b17e0d3
itakura yui <kaiamira26@gmail.com>
2018-08-06 23:53:22 +0900

    login/logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,33 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: session_params[:email])
+    if user && user.authenticate(session_params[:password])
+      login(user)
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to root_path
+  end
+
+  private
+  def login(user)
+    session[:user_id] = user.id
+  end
+
+  def logout
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  def session_params
+    params.require(:session).permit(:name, :email, :password)
+  end
+end

c9dcdc2cc21d20adb4e03a284ca9db5c1d3c32ac
itakura yui <kaiamira26@gmail.com>
2018-08-08 22:56:21 +0900

    email should be unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true
+  validates :email, presence: true, uniquness: true
   validates :name, presence: true
   validates :password, length: { minimum: 6 }
 end
